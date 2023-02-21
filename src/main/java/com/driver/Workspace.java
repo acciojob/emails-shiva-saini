72903cac37de95108ead254990b389816361e775
shiva-saini <shivasainisiyana2211@gmail.com>
2023-02-21 20:45:31 +0530

    Done3

--- src/main/java/com/driver/Workspace.java
+++ src/main/java/com/driver/Workspace.java
@@ -26,21 +26,61 @@ public class Workspace extends Gmail{
     }
 
     public int findMaxMeetings(){
-        ArrayList<Pair<LocalTime,LocalTime>> currCalendar = new ArrayList<>();
-        for(Meeting m :calendar){
-            currCalendar.add(Pair.of(m.getStartTime(),m.getEndTime()));
+        ArrayList<Pair<LocalTime,Integer>> currCalendar = new ArrayList<>();
+        for (int i = 0; i < calendar.size(); i++) {
+            currCalendar.add(Pair.of(calendar.get(i).getEndTime(), i));
         }
         Collections.sort(currCalendar);
-        if(calendar.size() == 0) return 0;
-        int cnt = 1;
-        Pair<LocalTime,LocalTime> p = currCalendar.get(0);
+        int cnt = 0;
+        if(!currCalendar.isEmpty()) {
+            cnt += 1;
+        }
+        LocalTime time_limit = currCalendar.get(0).getLeft();
         for(int i=1;i<currCalendar.size();i++){
-            if(currCalendar.get(i).getLeft().compareTo(p.getRight()) > 0){
-                cnt++;
-                p = currCalendar.get(i);
+            if (calendar.get(currCalendar.get(i).getRight()).getStartTime().compareTo(time_limit) > 0) {
+
+                cnt += 1;
+                time_limit = currCalendar.get(i).getLeft();
             }
         }
         return cnt;
+
+
+
+
+//        ArrayList<Pair<LocalTime, Integer>> endTimes = new ArrayList<>();
+//
+//        for (int i = 0; i < calendar.size(); i++) {
+//            endTimes.add(Pair.of(calendar.get(i).getEndTime(), i));
+//        }
+//
+//        Collections.sort(endTimes);
+//
+//        LocalTime time_limit = endTimes.get(0).getLeft();
+//
+//        int cnt = 0;
+//        if(!endTimes.isEmpty()) {
+//            cnt += 1;
+//        }
+//
+//        for (int i = 1; i < endTimes.size(); i++) {
+//            if (calendar.get(endTimes.get(i).getRight()).getStartTime().compareTo(time_limit) > 0) {
+//
+//                cnt += 1;
+//                time_limit = endTimes.get(i).getLeft();
+//            }
+//        }
+//
+//        return cnt;
+
+
+
+
+
+
+
+
+
         // find the maximum number of meetings you can attend
         // 1. At a particular time, you can be present in at most one meeting
         // 2. If you want to attend a meeting, you must join it at its start time and leave at end time.
